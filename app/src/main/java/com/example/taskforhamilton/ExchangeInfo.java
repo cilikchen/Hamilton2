eaa380a6ea05f57796629136d140eb17c349926e
cilikchen <cilik.chen612@gmail.com>
2022-07-17 16:45:24 +0100

    test for push

--- app/src/main/java/com/example/taskforhamilton/ExchangeInfo.java
+++ app/src/main/java/com/example/taskforhamilton/ExchangeInfo.java
@@ -4,7 +4,7 @@ public class ExchangeInfo {
     private final String exchangeFrom;
     private final String exchangeTo;
     private final int amount;
-
+    // test for push
     public ExchangeInfo(String exchangeFrom, String exchangeTo, int amount) {
         this.exchangeFrom = exchangeFrom;
         this.exchangeTo = exchangeTo;
